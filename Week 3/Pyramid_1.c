3e756ab65e71648ef6c6aeff7682e486f0c66ca9
Takdanai <jetak55.tak@gmail.com>
2022-08-20 14:31:56 +0700

    Week 3

---
+++ Week 3/Pyramid_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int row ; 
+    printf("Enter rows of pyramid : ");
+    scanf("%d",&row);
+
+    for(int r = 1 ; r<= row ; r++)
+    {
+        for(int s = row-r ; s >=0 ; s--)
+        {
+            printf(" ");
+        }
+        for(int p = 1 ; p <= 2*r - 1 ; p++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ Week 3/Pyramid_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n ,left=0 , right = 0;
+    printf("Enter rows of pyramid : ");
+    scanf("%d",&n);
+
+    for (int row = 1 ; row <= n ; row++,right++ , left--) 
+    {
+        for (int i = -n ; i <= n ; i++)
+            if(i >= left && i <= right )
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+            printf("\n");
+    }
+    
+    return 0;
+}
